9d63d3ab00545afabc3e2971a0205be4ee77c24c
yz201906 <yinzhou1989@gmail.com>
2020-07-26 20:00:01 -0400

    finished supervised learning/prediction

--- ST558_yz_shinyapp/app.R
+++ ST558_yz_shinyapp/app.R
@@ -3,10 +3,16 @@ library(tidyverse)
 library(DT)
 library(shinycssloaders)
 library(caret)
+library(olsrr)
+library(mltools)
+library(data.table)
+library(parallel)
+library(doParallel)
 
 seer18_sub <- read_rds("seer18_sub.rds")
 seer18_sub <- na.omit(seer18_sub)
-variables <- noquote(c("Year_of_diagnosis", "Age_recode_with_1_year_olds", "Laterality", "Race_recode_W_B_AI_API", "Grade"))
+variables <- noquote(c("age_group", "Laterality", "Race_recode_W_B_AI_API", "Grade"))
+use_cores <- detectCores() - 1
 
 # Functions
 desc_stats <- function (Var1) {
@@ -22,138 +28,134 @@ desc_stats <- function (Var1) {
     colnames(sum_table) <- c("Min.", "1st Qu.", "Median", "Mean", "SD", "3rd Qu.", "Max.", "N")
     return(sum_table)
 }
+encode_data <- function (df){
+    df[sapply(df, is.character)] <- lapply(df[sapply(df, is.character)], as.factor)
+    recoded <- one_hot(data.table(df))
+}
 
 # Define UI for random distribution app ----
 ui <- fluidPage(
-            # Sidebar layout with input and output definitions ----
-            sidebarLayout(
-                # Sidebar panel for inputs ----
-                sidebarPanel(width = 3,
-                    h3("Data exploration", style = "font-size: 18px;"),
-                    # Select training/testing data split ratio
-                    selectInput('data_ratio', 'Training/testing data ratio(%):', choices = c("90/10", "80/20", "70/30", "60/40"), selected = "80/20"),
-                    # Select a table to explore
-                    selectInput('desc_table', 'Descriptive Summary Stat for:', choices = c("Full data", "Train data", "Test data"), selected = "Full data"),
-                    selectInput('selected_variable', 'Variable for plotting:', choices = variables, selected = NULL),
-                    checkboxInput("sex", h4("Visualize by sex", style = "color:red;font-size: 15px;"), FALSE),
-                    # Data modeling section
-                    h3("Modeling parameters", style = "font-size: 18px;"),
-                    selectInput('trainMethod', 'Training method:', choices = c("bagTree", "randForest"), selected = "bagTree"),
-                    sliderInput('n_tree', "Adjust ntree values", min = 50, max = 500, step = 50, value = 100),
-                    uiOutput("m_try"),
-                    actionButton("train_model", "Train Model"),
-                    # Data prediction section
-                    h3("Predictions", style = "font-size: 18px;")
-                ),
-                
-                # Main panel for displaying outputs ----
-                mainPanel(width = 9,
-                    tabsetPanel(type = "tabs", id = "inTabset",
-                        tabPanel("Data summary",
-                                 textOutput("summary_title"),
-                                 tableOutput("table_summary"),
-                                 h1("Sampled training data table", style = "font-size:20px;"),
-                                 dataTableOutput("train_data_table"),
-                                 downloadButton("download_full_table", "Download full table"),
-                                 downloadButton("download_sampled_table", "Download sample table"),
-                                 tags$head(tags$style("#summary_title{font-size: 20px;}"))),
-                        tabPanel("Visualization",
-                                plotOutput('bar_plot') %>% withSpinner(color = "#0dc5c1"),
-                                conditionalPanel(condition = "input.sex == 1",
-                                    plotOutput('plot_by_sex') %>% withSpinner(color = "#0dc5c1")
-                                )
-                        ),
-                        tabPanel("Modeling",
-                                
-                        )
-                    )
-                )
-            )
+    # Sidebar layout with input and output definitions ----
+    sidebarLayout(
+        # Sidebar panel for inputs ----
+        sidebarPanel(width = 3,
+                     h3("Data exploration", style = "font-size: 18px;"),
+                     # Select training/testing data split ratio
+                     selectInput('data_year', 'Year of data:', choices = unique(seer18_sub$Year_of_diagnosis), selected = "2005"),
+                     selectInput('selected_variable', 'Variable for plotting:', choices = variables, selected = NULL),
+                     checkboxInput("sex", h4("Visualize by sex", style = "color:red;font-size: 15px;"), FALSE),
+                     # Data modeling section supervised
+                     h3("Modeling parameters (Supervised)", style = "font-size: 18px;"),
+                     selectInput('trainMethod', 'Training method:', choices = c("mlr", "bagTree"), selected = "mlr"),
+                     uiOutput("n_tree_slider"),
+                     uiOutput("cv_fold_slider"),
+                     uiOutput("cv_repeat_slider"),
+                     actionButton("train_model_sup", "Train Model"),
+                     # Data modeling section unsupervised
+                     h3("Modeling parameters (Unsupervised)", style = "font-size: 18px;"),
+                     actionButton("train_model_unsup", "Train Model")
+        ),
+        
+        # Main panel for displaying outputs ----
+        mainPanel(width = 9,
+                  tabsetPanel(type = "tabs", id = "inTabset",
+                              tabPanel("Data summary",
+                                       textOutput("summary_title"),
+                                       tableOutput("table_summary"),
+                                       h1("Sampled training data table", style = "font-size:20px;"),
+                                       dataTableOutput("train_data_table"),
+                                       downloadButton("download_full_table", "Download yearly table"),
+                                       downloadButton("download_sampled_table", "Download train data table"),
+                                       tags$head(tags$style("#summary_title{font-size: 20px;}")
+                                       )
+                              ),
+                              tabPanel("Visualization",
+                                       plotOutput('bar_plot') %>% withSpinner(color = "#0dc5c1"),
+                                       downloadButton("download_plot1", "Download plot"),
+                                       conditionalPanel(condition = "input.sex == 1",
+                                                        plotOutput('plot_by_sex') %>% withSpinner(color = "#0dc5c1"),
+                                                        downloadButton("download_plot2", "Download plot")
+                                       )
+                              ),
+                              tabPanel("Modeling(Supervised)",
+                                      verbatimTextOutput("mlr_sum") %>% withSpinner(color = "#0dc5c1"),
+                                      verbatimTextOutput("bt_sum") %>% withSpinner(color = "#0dc5c1"),
+                                      span(textOutput("pred_result"), style = "color:red;font-size: 15px;font-weight: bold;"),
+                                      dataTableOutput("test_data_table"),
+                                      downloadButton("download_test_table", "Download test data")
+                              ),
+                              tabPanel("Modeling(Unsupervised)",
+                                       
+                              )
+                  )
         )
+    )
+)
 
 
 
 
 
 
-
-
-
-
-# Define server logic for random distribution app ----
 server <- function(input, output, session) {
+    year_data <- reactive({
+        seer18_sub %>% filter(Year_of_diagnosis==input$data_year)
+    })
     # Data splitting into train and test sets
-    train_index <- eventReactive(input$data_ratio, {
-        if (input$data_ratio == "90/10") {
-            prob <- .9
-        } else if (input$data_ratio == "80/20") {
-            prob <- .8
-        } else if (input$data_ratio == "70/30") {
-            prob <- .7
-        } else {
-            prob <- .6
-        }
-        train_index <- createDataPartition(seer18_sub$Survival_months,
-                                           p = prob,
+    train_index <- reactive({
+        train_index <- createDataPartition(sample_train_data()$Survival_months,
+                                           p = .7,
                                            list = FALSE,
                                            times = 1)
         train_index <- as.vector(train_index)
     })
     train_data <- reactive({
-        train_data <- seer18_sub[train_index(), ]
+        train_data <- sample_train_data()[train_index(), ]
     })
     test_data <- reactive({
-        test_data <- seer18_sub[-train_index(), ]
+        test_data <- sample_train_data()[-train_index(), ]
     })
     # Output 5-number statistics for survived months
-    selected_data <- reactive({
-        if (input$desc_table == "Full data") {
-            data <- seer18_sub
-        } else if (input$desc_table == "Train data") {
-            data <- train_data()
-        } else {
-            data <- test_data()
-        }
-        data
-    })
     selected_table <- reactive({
-        summary_table <- desc_stats(selected_data()$Survival_months)
+        summary_table <- desc_stats(year_data()$Survival_months)
     })
     
     # Output descriptive summary statistics
-    output$summary_title <- renderText(paste0("Descriptive summary statistics for ", input$desc_table))
+    output$summary_title <- renderText(paste0("Descriptive summary statistics for months survived in ", input$data_year))
     output$table_summary <- renderTable(selected_table())
     # Output DT for sample training data
     sample_train_data <- reactive({
-        sample_n(train_data(), 100000)
+        sample_n(year_data(), 50000)
     })
     output$train_data_table <-
-        renderDataTable(sample_train_data(), class = "display nowrap compact", caption="Table displayed shows only 100000 randomly sampled rows. Please note that the full table is ~270MB.", options = list(pageLength = 20), filter = "top")
+        renderDataTable(sample_train_data(), class = "display nowrap compact", caption="Table displayed shows only 50000 randomly sampled rows.", options = list(pageLength = 20), filter = "top")
     # Download data tables
     output$download_full_table <- downloadHandler(
-        filename = 'train_data_full_table.csv',
+        filename = 'full_table.csv',
         content = function(file) {
-            write.csv(train_data(), file, row.names = FALSE)
+            write.csv(year_data(), file, row.names = FALSE)
         }
     )
     output$download_sampled_table <- downloadHandler(
         filename = 'train_data_sampled_table.csv',
         content = function(file) {
-            write.csv(sample_train_data(), file, row.names = FALSE)
+            write.csv(train_data(), file, row.names = FALSE)
         }
     )
-    # Render plots
+    # Render plots and allows downloading plots
     bar_plot_data <- reactive({
         if (input$sex == FALSE){
-            plot_vars <- selected_data() %>% select(Survival_months, input$selected_variable) 
+            plot_vars <- year_data() %>% select(Survival_months, input$selected_variable) 
             colnames(plot_vars) <- c("Survival_months", "Variable")
             to_plot <- plot_vars %>% group_by(Variable) %>% summarise(mean(Survival_months))
+            to_plot$Variable <- as.factor(to_plot$Variable)
             p1 <- ggplot(data = to_plot, aes(x = `mean(Survival_months)`)) + ylab("Months survived (Avg)") +
                 ylab(input$selected_variable) + geom_col(aes( y = Variable))
         } else {
-            plot_vars <- selected_data() %>% select(Survival_months, input$selected_variable, Sex_no_total) 
+            plot_vars <- year_data() %>% select(Survival_months, input$selected_variable, Sex_no_total) 
             colnames(plot_vars) <- c("Survival_months", "Variable", "Sex")
             to_plot <- plot_vars %>% group_by(Variable, Sex) %>% summarise(mean(Survival_months))
+            to_plot$Variable <- as.factor(to_plot$Variable)
             p1 <- ggplot(data = to_plot, aes(x = `mean(Survival_months)`)) + ylab("Months survived (Avg)") +
                 ylab(input$selected_variable) + geom_col(aes( y = Variable, fill = Sex), position="dodge")
         }
@@ -163,30 +165,131 @@ server <- function(input, output, session) {
         bar_plot_data()
     })
     box_plot_data <- reactive({
-        plot_vars <- selected_data() %>% select(Survival_months, Sex_no_total) 
+        plot_vars <- year_data() %>% select(Survival_months, Sex_no_total) 
         colnames(plot_vars) <- c("Survival_months", "Sex")
         p1 <- ggplot(data = plot_vars) + ylab("Months survived") +
             xlab("Sex") + geom_boxplot(aes(x = Sex, y = Survival_months))
         p1
     })
+    
+    output$download_plot1 <- downloadHandler(
+        filename = 'by_variable.png',
+        content = function(file) {
+            device <- function(..., width, height) {
+                grDevices::png(..., width = width, height = height, res = 300, units = "in")
+            }
+            ggsave(file, plot = bar_plot_data(), device = device)
+        }
+    )
     output$plot_by_sex <- renderPlot({
         box_plot_data()
     })
+    output$download_plot2 <- downloadHandler(
+        filename = 'by_variable.png',
+        content = function(file) {
+            device <- function(..., width, height) {
+                grDevices::png(..., width = width, height = height, res = 300, units = "in")
+            }
+            ggsave(file, plot = box_plot_data(), device = device)
+        }
+    )
     observeEvent(input$sex, {
         updateTabsetPanel(session, "inTabset",
                           selected = "Visualization")
     })
-    # Modeling page
+    # Modeling pages
     # Jump to modeling tab if clicked
-    observeEvent(input$train_model, {
+    observeEvent(input$train_model_sup, {
         updateTabsetPanel(session, "inTabset",
-                          selected = "Modeling")
+                          selected = "Modeling(Supervised)")
     })
-    output$m_try <- renderUI({
-        if (input$trainMethod=="randForest") {
-            sliderInput('m_try', "mtry tuning", min = 1, max = 9, step = 1, value = 3)
+    observeEvent(input$train_model_unsup, {
+        updateTabsetPanel(session, "inTabset",
+                          selected = "Modeling(Unsupervised)")
+    })
+    output$n_tree_slider <- renderUI({
+        if (input$trainMethod=="bagTree") {
+            sliderInput('n_tree', "Adjust ntree values", min = 50, max = 500, step = 50, value = 100)
         }
     })
+    output$cv_fold_slider <- renderUI({
+        if (input$trainMethod=="bagTree") {
+            sliderInput('cv_fold', "Adjust # of CV folds", min = 5, max = 15, step = 1, value = 10)
+        }
+    })
+    output$cv_repeat_slider <- renderUI({
+        if (input$trainMethod=="bagTree") {
+            sliderInput('cv_repeat', "Adjust # of CV repeats", min = 1, max = 5, step = 1, value = 3)
+        }
+    })
+    #Prep data for modeling
+    recoded_data <- reactive({
+        my_data <- train_data() %>% select(!Year_of_diagnosis)
+        my_data <- encode_data(my_data)
+        as.data.frame(my_data)
+    })
+    # MLR
+    # modeling
+    mlr_trained <- eventReactive(input$train_model_sup, {
+        if (isolate(input$trainMethod)=="mlr") {
+            linear_model_fit <- lm(Survival_months ~ ., isolate(recoded_data()))
+            linear_model <-step((linear_model_fit), direction = "both", trace = 0)
+        }
+    })
+    observeEvent(input$train_model_sup, {
+        output$mlr_sum <- renderPrint({
+            if (isolate(input$trainMethod)=="mlr") {
+                summary(mlr_trained())
+            }
+        })
+    })
+    
+    # bagged tree
+    bt_trained <- eventReactive(input$train_model_sup,{
+        if (isolate(input$trainMethod)=="bagTree") {
+            cl <- makePSOCKcluster(use_cores)
+            registerDoParallel(cl)
+            bt_fit <- train(Survival_months ~ ., isolate(recoded_data()), method = "gbm", verbose = 0,
+                            preProcess=c("center", "scale"), trControl = trainControl(method = "repeatedcv", number = isolate(input$cv_fold), repeats = isolate(input$cv_repeat), allowParallel = TRUE, verboseIter = FALSE),
+                            tuneGrid = expand.grid(n.trees = isolate(input$n_tree), interaction.depth = c(1,4,9),
+                                                   shrinkage = 0.1, n.minobsinnode = 10))
+            stopCluster(cl)
+            bt_fit
+        }
+    })
+    observeEvent(input$train_model_sup, {
+        output$bt_sum <- renderPrint({
+            if (isolate(input$trainMethod)=="bagTree") {
+                bt_trained()
+            }
+        })
+    })
+    
+    # Prediction
+    output$test_data_table <- renderDataTable(test_data(), class = "display nowrap compact", caption="Please select a row for prediction.", options = list(pageLength = 5, sDom  = '<"top">lrt<"bottom">ip'), selection = 'single', filter = "top")
+    output$download_test_table <- downloadHandler(
+        filename = 'test_data.csv',
+        content = function(file) {
+            write.csv(test_data(), file, row.names = FALSE)
+        }
+    )
+    selected_test_data <- eventReactive(input$test_data_table_rows_selected, {
+        my_data <- test_data() %>% select(!Survival_months)
+        my_data <- encode_data(my_data) %>% slice(input$test_data_table_rows_selected)
+        as.data.frame(my_data)
+    })
+    observeEvent(input$test_data_table_rows_selected, {
+        output$pred_result <- renderText({
+            if (isolate(input$trainMethod)=="mlr") {
+                months <- predict(mlr_trained(), newdata = selected_test_data())[1]
+                paste0("The predicted number of months of survival based on selected case is: ", round(months,digits = 0))
+            } else {
+                months <- predict(bt_trained(), newdata = selected_test_data())[1]
+                paste0("The predicted number of months of survival based on selected case is: ", round(months,digits = 0))
+            }
+        })
+    })
+
 }
 
 
